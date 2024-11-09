3055e3a42b153df8b2f102102573752cba044e66
Abukar-1000 <abukardjama1@gmail.com>
2024-11-09 04:18:24 -0500

    Added all routes, request types, and ensured necissary routes recieved an NPM package as a zipped file

--- Server/index.ts
+++ Server/index.ts
@@ -1,28 +1,88 @@
-import express, { Request, Response } from 'express';
+import express, { Request, response, Response } from 'express';
 import multer from 'multer';
 import unzipper from 'unzipper';
-import * as PakageRating from "./types/RatePackageRequest"
+import path from 'path';
 import * as scoreMethod from "../MVP/src/Scoring/scoring"
 import * as processMethod from "../MVP/src/Processors/urlProcessor"
 import * as sanitize from "../MVP/src/Input/Sanitize"
 
+import UploadPackageRequest from './types/Request/UploadPackageRequest';
+import CheckPackageRatingRequest from './types/Request/CheckPackageRatingRequest';
+import UpdatePackageRequest from './types/Request/UpdatePackageRequest';
+import DownloadPackageRequest from './types/Request/DownloadPackageRequest';
+import FetchAvailableVersionsRequest from './types/Request/FetchAvailableVersionsRequest';
+import IngestPackageRequest from './types/Request/IngestPackageRequest';
+import FetchPackageDirectoryRequest from './types/Request/FetchPackageDirectoryRequest';
+import SearchPackagesRequest from './types/Request/SearchPackagesRequest';
+import CheckSizeCostRequest from './types/Request/CheckSizeCostRequest';
+
 const app = express();
 const PORT = 3000;
 
-
-const upload = multer({ dest: 'uploads/' });
-
+// Configuration for handling zipped files
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/'); // folder to store uploaded files temporarly untill Google cloud storage is configured
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${file.originalname}`); // rename file
+    }
+});
+  
+// Multer middleware to handle single file upload with 'zipFile' as the field name
+// This is place holder untill GCP or S3 is used for keeping track of the data
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        const ext = path.extname(file.originalname).toLowerCase();
+        if (ext === '.zip') {
+            // Extract package name here
+            cb(null, true); // Accept .zip files only
+        } else {
+            // reject file here 
+            cb(null, false); // Reject other files
+        }
+    }
+});
+  
 // Middleware to parse JSON
 app.use(express.json());
-app.use('/packages/rating', upload.single('zipfile'));
 
+app.post(
+    '/package/:debloat', 
+    upload.single('package'),
+    async (req: UploadPackageRequest, res: Response) => {
+        console.log(req.params, req.body);
+        const filename = req.file?.filename;
 
-/** rates recieved package
- * Test with postman using: http://localhost:3000/packages/rating/:packageName/:version?packageName=name&version=1.0
-*/
+        // zipped file in uploads folder 
+        // needs debloat to be implemented
+        const endPointResponse = {
+            params: req.params,
+            body: req.body,
+            filename: filename
+        };
+        res.status(200).send(endPointResponse);
+});
+
+app.post(
+    '/packages/:packageName/:version',
+    upload.single('package'),
+    async (req: UpdatePackageRequest, res: Response) => {
+        
+        const filename = req.file?.filename || "Didnt get a zipped file";
+        const endPointResponse = {
+            params: req.params,
+            body: req.body,
+            filename: filename
+        };
+        res.status(200).send(endPointResponse);
+});
+
 app.post(
     '/packages/rating/:packageName/:version', 
-    async (req: Request<PakageRating.RatePackageRequestParams, any, PakageRating.RatePackageRequestBody>, res: Response) => 
+    upload.single('package'),
+    async (req: CheckPackageRatingRequest, res: Response) => 
 {
     try {
 
@@ -41,7 +101,6 @@ app.post(
         res.status(200).send(JSON.stringify(npmPkgScore));
 
         // res.status(200).send('File uploaded and extracted successfully');
-
         // // Optional: Unzip the file to a specific folder
         // const outputDir = path.join(__dirname, 'extracted');
         // fs.createReadStream(zipFilePath)
@@ -54,6 +113,123 @@ app.post(
     }
 });
 
+/**
+ * was originally: /packages/{packageName}/{version} in plan
+ * changed to:
+ *  /packages/download/{packageName}/{version}
+ */
+app.get(
+    '/packages/download/:packageName/:version',
+    async (req: DownloadPackageRequest, res: Response) => {
+        const endPointResponse = {
+            params: req.params,
+            body: req.body
+        };
+        res.status(200).send(endPointResponse);
+    }
+)
+
+app.get(
+    '/packages/:packageName/versions/:version/:versionRange',
+    async (req: FetchAvailableVersionsRequest, res: Response) => {
+        const endPointResponse = {
+            params: req.params,
+            body: req.body
+        };
+        res.status(200).send(endPointResponse);
+    }
+)
+
+app.post(
+    '/ingest/:packageName/:version',
+    async (req: IngestPackageRequest, res: Response) => {
+        const endPointResponse = {
+            params: req.params,
+            body: req.body
+        };
+        res.status(200).send(endPointResponse);
+    }
+)
+
+app.post(
+    '/ingest/:packageName/:version',
+    async (req: IngestPackageRequest, res: Response) => {
+        const endPointResponse = {
+            params: req.params,
+            body: req.body
+        };
+        res.status(200).send(endPointResponse);
+    }
+)
+
+app.get(
+    '/packages/:page',
+    async (req: FetchPackageDirectoryRequest, res: Response) => {
+        /**
+         * I removed limit from the request parameters.
+         * Its a vulnurability if they are able to control the amt 
+         * of packages on a given response.
+         * 
+         * Calcualte the given entries by page number and a fixed page
+         * size only on the server
+         */
+
+        const pageSize = 30;
+        const startIndex = (pageSize * req.params.page);
+        const stopIndex = startIndex + pageSize;
+        
+        const endPointResponse = {
+            params: req.params,
+            body: req.body
+        };
+
+        res.status(200).send(endPointResponse);
+    }
+)
+
+app.get(
+    '/packages/search/:nameRegex/:readmeRegex',
+    async (req: SearchPackagesRequest, res: Response) => {
+        /**
+         * Passing in raw regex over the network will fail since it will mess with the URL.
+         *  -urls dont allow you to use: [] () / _
+         * Possiably hash the regex with SHA256 before sending it and unhash it before using it.
+         */
+        const endPointResponse = {
+            params: req.params,
+            body: req.body
+        };
+
+        res.status(200).send(endPointResponse);
+    }
+)
+
+app.get(
+    '/packages/size-cost',
+    async (req: CheckSizeCostRequest, res: Response) => {
+        /**
+         * 
+         * Used the body of the request for the packages so there 
+         * can be an array of simple Package objects.
+         * 
+         * Each has a name and an array of versions
+         */
+        const endPointResponse = {
+            params: req.params,
+            body: req.body
+        };
+
+        res.status(200).send(endPointResponse);
+    }
+)
+
+app.post(
+    '/reset',
+    async (req: Request, res: Response) => {
+        
+        res.status(200).send("Reset endpoint hit.");
+    }
+)
 
 // Basic route
 app.get('/', (req: Request, res: Response) => {
---
+++ Server/types/Request/UploadPackageRequest.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import * as core from "express-serve-static-core";
+
+export default interface UploadPackageRequest<
+    P = core.ParamsDictionary, 
+    ResBody = any,
+    ReqBody = {
+        package: Blob // This is the zipped file that will sent by the client. TS defined it as a blob 
+    },
+    ReqQuery = core.Query,
+    Locals extends Record<string, any> = Record<string, any>,
+> extends core.Request<P, ResBody, ReqBody, ReqQuery, Locals> {}
+
