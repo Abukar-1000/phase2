3643f41d4fd04cd4918f87dcf061e27d80d920bb
Abukar-1000 <abukardjama1@gmail.com>
2024-11-24 02:13:40 -0500

    updated server address

--- Server/index.ts
+++ Server/index.ts
@@ -236,6 +236,6 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello, TypeScript with Express!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
